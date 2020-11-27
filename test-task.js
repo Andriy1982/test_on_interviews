6e1bb1050d61bbe5c7d3aa0af016ae45764124ea
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-11-27 08:45:17 +0200

    new test

--- test-task.js
+++ test-task.js
@@ -9,20 +9,31 @@
 // "example" => "xampl"
 
 const getElementBetweenUnique = (arr) => {
-    const notUniqueEl = arr.find((el, index, array) => {
-        return array.indexOf(el) !== index
-    })
 
-    console.log(arr.indexOf(notUniqueEl));
-    const result = arr.slice(arr.indexOf(notUniqueEl) + 1)
-    console.log(result.slice(0, result.indexOf(notUniqueEl)));
+    // if(typeof arr === "string") {
+    //     arr = arr.split('');
+    //     console.log(arr);
+    // }
+
+    
+    
+    // console.log(Object.prototype.toString.call(arr))
+    // const notUniqueEl = arr.find((el, index, array) => {
+    //     return array.indexOf(el) !== index
+    // })
+
+    // console.log(arr.indexOf(notUniqueEl));
+    // const result = arr.slice(arr.indexOf(notUniqueEl) + 1)
+    // console.log(result.slice(0, result.indexOf(notUniqueEl)));
+    // console.log(arr = arr.join(arr));
     // let start;
     // let end;
 
-    // for (let i = 0; i < arr.length; i += 1) {
-    //     a
-    // }
+    for (let i = 0; i < arr.length; i += 1) {
+        console.log(arr.slice(arr.indexOf(arr[i])));
+        // if (arr.includes(arr.slice()))
+    }
 
 }
 
-getElementBetweenUnique([0, 0] )
+getElementBetweenUnique([0, 9, 1, 2, 3, 4, 5, 6, 1, 7, 8])
